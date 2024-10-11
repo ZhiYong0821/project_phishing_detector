19c247047833f0fdaa68175734fa87c59fdc73cb
Wei Seng <2401493@sit.singaporetech.edu.sg>
2024-10-11 23:32:37 +0800

    addition of comments and cleaning up

--- src/data_prep.py
+++ src/data_prep.py
@@ -40,24 +40,24 @@ def extract_email_features(filepath):
             body = msg.get_payload(decode=True).decode(msg.get_content_charset() or 'utf-8', errors='ignore')
         except:
             body = msg.get_payload(decode=False)
-    # returns a dictionary with all of the relevant parts of content from the emails
+    # Returns a dictionary with all of the relevant parts of content from the emails
     return {
         'subject': subject,
         'from': sender,
         'body': body,
     }
 
-# prepares and organize the collected emails into a structured format
+# Prepares and organize the collected emails into a structured format
 def prepare_dataset(base_directory):
     data = []
-    # sort emails according to category of the emails (e.g: phishing/non-phishing email)
+    # Sort emails according to category of the emails (e.g: phishing/non-phishing email)
     categories = {
         'easy_ham': 0, # legitimate emails (easy to tell)
         'hard_ham': 0, # legitimate emails (harder to tell but still are legit emails)
         'spam': 1 # Confirmed phishing/spam emails
     }
 
-    # starts a loop which run through each category in the dictionary, processing each category of emails at a time.
+    # Starts a loop which run through each category in the dictionary, processing each category of emails at a time.
     for category, is_phishing in categories.items():
         directory = os.path.join(base_directory, category)
         # process and join each file in each email category directory
@@ -70,23 +70,23 @@ def prepare_dataset(base_directory):
                 features['is_phishing'] = is_phishing # adds the label accordingly
                 data.append(features)
 
-    # after processing all emails, returns the list of email (in form of dictionaries) to a pandas dataframe
+    # After processing all emails, returns the list of email (in form of dictionaries) to a pandas dataframe
     return pd.DataFrame(data)
 
 # Changed base_dir code cater to different file paths - YongYing
 base_dir = os.path.dirname(os.path.abspath(__file__))
 data_file_path = os.path.join(base_dir,'datasets')
 
-# assigns the list of email dictionaries to dataframe variable
+# Assigns the list of email dictionaries to dataframe variable
 df = prepare_dataset(data_file_path)
 
-# drops missing values, if any
+# Drops missing values, if any
 df.dropna()
 
 # Optionally check again for any remaining missing values
 print("Missing values in each column:\n", df.isnull().sum())
 
-# converts prepared dictionary dataset to a CSV file
+# Converts prepared dictionary dataset to a CSV file
 df.to_csv('boss_email_dataset_encode_change_final.csv', index=False, encoding='utf-8', na_rep='')
 
 print(f"Dataset prepared and saved to current directory.")
